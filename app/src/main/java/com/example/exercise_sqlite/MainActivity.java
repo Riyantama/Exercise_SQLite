f0c804033dde02318dd6b480606b11d85d7a2fcd
Riyantama <riyantama2016@gmail.com>
2020-04-20 20:54:35 +0700

    initial commit

---
+++ app/src/main/java/com/example/exercise_sqlite/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.exercise_sqlite;
+
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
